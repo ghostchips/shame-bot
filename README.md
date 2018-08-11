6f2cc423fe11e0ffac9d38b521bf0141570a6ef9
Micah <micahhope@yahoo.com>
2018-08-11 19:02:28 +1000

    Updated README to have instructions

--- README.md
+++ README.md
@@ -1,4 +1,43 @@
 
-For a bit of fun, our office had a Wall of Shame set up through a simple Slack bot to keep track of each departments blunders. 
+## Wall of Shame-Bot
 
-However the existing Wall of Shame was accidentally deleted and is gone forever. This is a simple attempt to rebuild it.
+For a bit of fun, our office had a Wall of Shame to keep track of each departments blunders. Initially this was posted on a sheet of paper on the wall, but was later set up through a simple Slack bot on a private slack channel.
+
+However the Wall of Shame was accidentally deleted (_and not version controlled_) and is now gone forever. This is a simple attempt to rebuild it, with some added functionality.
+
+In addition to being able to shame people, the bot can keep track of teams and it's members, list rankings, add or remove teams and team members. It also has some simple validations and persists the data into a yaml file.
+
+> _Disclaimer: It was not my idea to have a wall of shame in the office. This was an existing practice and meant for some light-hearted banter between development and operations. I rebuilt it as a side project and was never actually reintroduced back into the office._
+
+### How to use
+
+
+First of all be sure to read how to set up a slack-bot on your slack channel. The ruby-slack-bot repo has a simple deployment guide to do this. You can find it [here](https://github.com/slack-ruby/slack-ruby-bot/blob/master/DEPLOYMENT.md).
+
+After you've cloned the repo create an .env file and add your slack api token under in a SLACK_API_TOKEN constant.
+
+Once you have created and invited your bot to your slack channel, run 'bunlde exec foreman start' to start your bot server. You then should be able to interact with your bot by typing out its @name followed by a command.
+
+###### Commands
+
+Type _help_ to see all available commands:
+
+- **add team**: Add a team to the Wall of Shame
+- **remove team**: Remove a team from the Wall of Shame
+- **add user**: Add user to a team
+- **remove user**: Remove user from a team
+- **shame**: Shame people for their embarrasing blunders
+- **list team shamings**: List all shamings from a team
+- **list user shamings**: List a users shamings
+- **list team rankings**: Display all teams ranked by their shamings count
+- **list user rankings**: Display all users ranked by their shamings count
+
+Subsequently type _help_ <_command_> to get more details.
+
+___
+
+#### To do:
+
+- Be able to use @username handles when adding and shaming
+- Add bot listener so you don't need @botname
+- Persist the yaml file to a google drive sheet
--- shamebot/bot.rb
+++ shamebot/bot.rb
@@ -1,53 +1,53 @@
 module ShameBot
   class Bot < SlackRubyBot::Bot
-    
+
     help do
       title 'Wall of Shame'
       desc  'Here are recorded those who have made embarassing blunders'
-      
-      command 'add user' do
-        desc 'Add user to a team'
-        long_desc 'type "add user user_name to team_name" to add a user to a team'
-      end
-      
-      command 'remove user' do
-        desc 'Remove user from a team'
-        long_desc 'type "remove user user_name" to remove from their respective team'
-      end
-      
+
       command 'add team' do
         desc 'Add a team to the Wall of Shame'
-        long_desc 'type "add team team_name" to add a team to the Wall of Shame'
+        long_desc 'type "add team "TEAM NAME"" to add a team to the Wall of Shame'
       end
-      
+
       command 'remove team' do
         desc 'Remove a team from the Wall of Shame'
-        long_desc 'type "remove team team_name" to remove a team from the Wall of Shame'
+        long_desc 'type "remove team "TEAM NAME"" to remove a team from the Wall of Shame'
       end
-      
+
+      command 'add user' do
+        desc 'Add user to a team'
+        long_desc 'type "add user \'USER NAME\' to \'TEAM NAME\' to add a user to a team'
+      end
+
+      command 'remove user' do
+        desc 'Remove user from a team'
+        long_desc 'type "remove user \'USER NAME"" to remove from their respective team'
+      end
+
       command 'shame' do
         desc 'Shame people for their embarrasing blunders'
-        long_desc 'type "shame user_name for reason" to shame to an existing user. Punctuate to beween reasons to add multiple at one time. Shamings cannot be deleted.'
+        long_desc 'type "shame \'USER NAME\' for reason" to shame to an existing user. Punctuate to beween reasons to add multiple at one time. Shamings cannot be deleted!'
       end
-      
-      command 'list user shamings' do
-        desc 'list a users shamings'
-        long_desc 'type "list user shamings for user_name" to see a users shamings'
-      end
-      
+
       command 'list team shamings' do
-        desc 'list all shamings from a team'
-        long_desc 'type "list team shamings for user_name" to see a list for a teams collective shamings'
+        desc 'List all shamings from a team'
+        long_desc 'type "list team shamings for \'USER NAME\' to see a list for a teams collective shamings'
       end
-      
-      command 'list user rankings' do
-        desc 'display all users ranked by their shamings count'
-        long_desc 'type "list user rankings" to see a list of all users sorted by their shaming count'
+
+      command 'list user shamings' do
+        desc 'List a users shamings'
+        long_desc 'type "list user shamings for \'USER NAME\' to see a users shamings'
       end
-      
+
       command 'list team rankings' do
-        desc 'display all teams ranked by their shamings count'
+        desc 'Display all teams ranked by their shamings count'
         long_desc 'type "list team rankings" to see a list of all teams sorted by their shaming count'
+      end
+
+      command 'list user rankings' do
+        desc 'Display all users ranked by their shamings count'
+        long_desc 'type "list user rankings" to see a list of all users sorted by their shaming count'
       end
     end
 
